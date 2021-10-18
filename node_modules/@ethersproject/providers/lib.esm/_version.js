5708ac4263efa0006afd7f780dba8f1ac0e56bc0
Dan Hepworth <djh58@duke.edu>
2021-10-18 15:48:33 -0400

    removed cahces

--- node_modules/@ethersproject/providers/lib.esm/_version.js
+++
@@ -1,2 +0,0 @@
-export const version = "providers/5.4.5";
-//# sourceMappingURL=_version.js.map
--- node_modules/@ethersproject/providers/lib/_version.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const version = "providers/5.4.5";
-//# sourceMappingURL=_version.d.ts.map
